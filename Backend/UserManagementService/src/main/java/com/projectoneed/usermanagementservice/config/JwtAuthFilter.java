93df92aa81d4521e11329e98e172986d53805e45
Sathursan-S <sathursan500@gmail.com>
2024-02-27 18:06:20 +0530

    impliment JwtService

---
+++ Backend/UserManagementService/src/main/java/com/projectoneed/usermanagementservice/config/JwtAuthFilter.java
@@ -0,0 +1,36 @@
+package com.projectoneed.usermanagementservice.config;
+
+import com.projectoneed.usermanagementservice.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+        if (authHeader == null && !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        } else {
+            jwt = authHeader.substring(7);
+            username = jwtService.extractUsername(jwt);
+        }
+    }
+}
