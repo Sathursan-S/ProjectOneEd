0017a8c5fe8f08b34d86ad92e59b21bdf644e6eb
Himosh <121594264+Himosh@users.noreply.github.com>
2024-03-18 00:58:03 +0530

    Delete ClassManagementService directory

--- ClassManagementService/src/main/java/com/projectoneed/ClassManagementService/repository/ClassTimeSlot.java
+++
@@ -1,6 +0,0 @@
-package com.projectoneed.ClassManagementService.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClassTimeSlot extends MongoRepository<ClassTimeSlot, Integer>{
-}
