79c03d5c00ef897d5d4afbd9d03525566f744568
Sathursan-S <sathursan500@gmail.com>
2024-04-15 06:07:25 +0530

    token table error fixed

--- Backend/AuthService/src/main/java/com/projectoneed/authservice/repository/TokenRepository.java
+++ Backend/AuthService/src/main/java/com/projectoneed/authservice/repository/TokenRepository.java
@@ -2,7 +2,9 @@ package com.projectoneed.authservice.repository;
 
 import com.projectoneed.authservice.model.token.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +19,19 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
   List<Token> findAllValidTokenByUser(String id);
 
   Optional<Token> findByToken(String token);
+
+//  @Query(value = """
+//      select t from Token t inner join User u\s
+//      on t.user.id = u.id\s
+//      where u.id = :id and t.expired = true\s
+//      """)
+//  List<Token> findAllExpiredTokensByUser(String id);
+//
+//  @Transactional
+//  @Modifying
+//  @Query(value = """
+//      delete from Token t\s
+//      where t.id in :ids
+//      """)
+//  void deleteTokensWithIds(List<Integer> ids);
 }
