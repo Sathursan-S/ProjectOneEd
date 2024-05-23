5f89885c991f3420a0609a8a2d22acee713fac67
Sathursan-S <sathursan500@gmail.com>
2024-05-24 00:21:09 +0530

    api gateway connected

--- Frontend/src/Api/AuthRequest.js
+++ Frontend/src/Api/AuthRequest.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.8.197:8081",
+  baseURL: "http://localhost:8080",
 });
-const DETAILSAPI = axios.create({
-  baseURL: "http://192.168.8.197:8082",
-});
+
 
 export const logIn = (formData) =>
   API.post("api/v1/auth/authenticate", formData);
 
 export const signUp = (formData) => API.post("/api/v1/auth/register", formData);
 export const fetchStudentDetails = (id) =>
-  DETAILSAPI.get(`/api/v1/student/dashboard/${id}`);
+  API.get(`/api/v1/student/dashboard/${id}`);
 export const fetchInstructorDetails = (id) =>
-  DETAILSAPI.get(`/api/v1/instructor/dashboard/${id}`);
+  API.get(`/api/v1/instructor/dashboard/${id}`);
